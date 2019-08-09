3f4ef62e9c6a39e5f035d1132cea6573804fc20a
Bogab <qhrkq@likelion.org>
2019-08-09 21:23:05 +0900

    fdf

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,1 @@
 from django.contrib import admin
-from .models import Blog
-
-admin.site.register(Blog)
--- stores/views.py
+++ stores/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here. blog_id
 def store(requests, pk):
-    return render(requests, 'index.html')
+    return render(requests, 'index1.html')
